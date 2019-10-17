6fca9ee2cdb3a7995fd5cf72677fa4c3a8fac9c6
github username <github email address>
2019-10-17 10:09:38 -0400

    added toggle component

---
+++ src/toggle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Toggle extends Component {
+  render(){
+    return (
+      <div>
+        <button></button>
+      </div>
+    )
+  }
+
+}
+
+export default Toggle
