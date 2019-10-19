69c9738885a6a2794b6c06dac8de35062b34a67d
github username <joanrigdon@gmail.com>
2019-10-18 21:38:19 -0400

    imported styled components, added modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './ToggleRPC'
+import Portal from './Portal'
+import Modal from './Modal'
 
 
 function App() {
@@ -10,14 +12,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Modal />
       <Toggle>
         {({ on, toggle }) => (
-        <div>
+        <>
           {on && <h1>Show Me</h1>}
           <button onClick={toggle}>Show / Hide </button>
-        </div>
+          <Portal>
+            {on && <h1>Hi I'm in a portal!</h1>}
+          </Portal>
+
+        </>
       )}
       </Toggle>
+
+
+
     </div>
   )
 
